bb8267d26a290a5126b1d8765cf792cf59e3170d
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2020-02-29 13:02:03 -0800

    updatedhomepage

--- exercisetracker/src/pages/Tracker.js
+++ exercisetracker/src/pages/Tracker.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 
 function About() {
   return (
+    <div>
+      <Hero backgroundImage="https://static.vecteezy.com/system/resources/previews/000/139/756/large_2x/heart-rate-running-free-vector.jpg">
+      </Hero>
           <div class="ui container workout">
             <div class="ui raised card m-auto">
                 <div class="prev-workout">
@@ -13,7 +17,7 @@ function About() {
                     <div class="workout-stats"></div>
                     <div class="buttons">
                       <a href="/exercise?" class="huge ui blue button">Continue Workout</a>
-                      <a href="/exercise" class="huge positive ui button">New Workout</a>
+                      <a href="/exercise?" class="huge positive ui button">New Workout</a>
                     </div>
                   </div>
                 </div>
@@ -21,6 +25,7 @@ function About() {
                   <h3>Create Your First Workout</h3>
                 </div>
               </div>
+        </div>
         </div>
   );
 }
